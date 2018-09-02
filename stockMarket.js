a94175ef76c12412c114378bf35e1c45e140a7d7
paulabarreto <paulavilaca@gmail.com>
2018-09-02 01:44:03 +0000

    pseudo code

---
+++ stockMarket.js
@@ -0,0 +1,14 @@
+// Create a function maxProfit, which, given a list of stock prices for a given day,
+// returns the maximum profit that could have been made by buying a stock at the given
+// price and then selling the stock later on. For example if the input is:
+// [45, 24, 35, 31, 40, 38, 11] then your program should return 16 because if you
+// bought the stock at $24 and sold it at $40, a profit of $16 was made and this is
+// the largest profit that could be made. If no profit could have been made, return -1.
+
+//Find the lowest number
+
+//compare with highest of higher index and store in an  array
+
+//Repeat until all there are no values left
+
+//get higher value in the array
